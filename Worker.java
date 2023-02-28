03263659af861dfea0f88a9fc6b99e5dd2eb6a7d
Kevin <k1den@rambler.ru>
2023-02-28 20:59:43 +0300

    Add and call errorCallback

--- Worker.java
+++ Worker.java
@@ -3,21 +3,24 @@ public class Worker {
     public interface OnTaskDoneListener {
         void onDone(String result);
     }
+
     private OnTaskDoneListener callback;
 
     @FunctionalInterface
     public interface OnTaskErrorListener {
         void onError(String result);
     }
+
     private OnTaskErrorListener errorCallback;
 
     public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
         this.errorCallback = errorCallback;
     }
+
     public void start() {
         for (int i = 0; i < 100; i++) {
-            if (i==33)
+            if (i == 33)
                 errorCallback.onError("Task " + i + " has error");
             else callback.onDone("Task " + i + " is done");
         }
